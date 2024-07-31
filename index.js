b8cb4eeb7d0a6e060fc008cfc101460703b8d10a
Chackochan Boby <chackoakasalayil@gmail.com>
2024-07-31 19:46:11 +0530

    configured cors to include methods allowedHeaders

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const { mongoose } = require("mongoose");
 //use cors for every routes
 app.use(cors({
   credentials: true,
-  origin:process.env.ENVIRONMENT==="development"?["http://localhost:5173","http://localhost:5174"]:["https://library-frontend-ruddy.vercel.app/"]
+  origin: process.env.ENVIRONMENT === "development"
+    ? ["http://localhost:5173", "http://localhost:5174"]
+    : ["https://library-frontend-ruddy.vercel.app"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 //get body of requests
